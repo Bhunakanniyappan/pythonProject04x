8c8dfae4e8df8620237f3ed6d7ce2c438ada84cc
Bhunakanniyappan <bhuna11111@gmail.com>
2024-08-14 16:20:05 +0530

    task 1 13082024

---
+++ src/ex_13082024/lab005.py
@@ -0,0 +1,27 @@
+#Task #1
+# Home Can you create a Program I will give you number(userinput and print table)
+#f"{}" String format concept
+#User input - num int -> 10, 100, -1, 2, 3.14 -> input
+#9x1 = 9
+#9x2 = 18... till 10
+num=int(input("Enter the number"))
+table = 9
+print(f"{table}*{num}={table}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
+num=int(input("Enter the number"))
+print(f"{table}*{num}={table*num}")
---
+++ src/ex_13082024/lab006.py
@@ -0,0 +1,15 @@
+#Task #2
+#Create a program , take 2 inputs from the user num1, num2 and give them
+#max
+#pow num1 to num2
+#sub, mul, sum, div.
+#Format your out with f{""}
+
+num1= int(input("Enter The Number 1"))
+num2=int(input("Enter The Number 2"))
+print("Sum is",num1+num2)
+print("Sub is",num1-num2)
+print("Mul is",num1*num2)
+print("Div is",num1/num2)
+print("pow is",pow(num1,num2))
+print("max is",max(num1,num2))
